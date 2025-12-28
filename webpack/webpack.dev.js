b351e1ff297b3d0a0f0a215ac72a91ae5355d34d
affner <affner.mendoza@gmail.com>
2025-12-28 01:15:17 -0600

    cambios de apariencia

--- webpack/webpack.dev.js
+++ webpack/webpack.dev.js
@@ -98,7 +98,7 @@ module.exports = async options =>
       ),
       new WebpackNotifierPlugin({
         title: 'Monsterdam Chicks',
-        contentImage: path.join(__dirname, 'logo-jhipster.png'),
+        contentImage: path.join(__dirname, 'logo-monsterdam.png'),
       }),
     ].filter(Boolean),
   });
